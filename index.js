d3e2ca434da3bc4277ecd481e990ec110d946a4e
lee <lyhdra99@svcorps.dev>
2020-06-04 20:01:21 +0900

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native'
-import DatePickerIOS from './DatePickerIOS'
-import DatePickerAndroid from './DatePickerAndroid'
+import DatePickerIOS from './index.ios'
+import DatePickerAndroid from './index.android'
 import propTypes from './propTypes'
 import defaultProps from './defaultProps'
 
