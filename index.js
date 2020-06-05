307756ff8d848b3518ed1588c80e6fd02edea00c
lee <lyhdra99@svcorps.dev>
2020-06-05 21:57:22 +0900

    init

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { Platform } from 'react-native'
 import DatePickerIOS from './index.ios'
 import DatePickerAndroid from './index.android'
-import propTypes from './propTypes'
-import defaultProps from './defaultProps'
+// import propTypes from './propTypes'
+// import defaultProps from './defaultProps'
 
 const RNInfiniteScroll = Platform.select({
   android: DatePickerAndroid,
   ios: DatePickerIOS,
 })
 
-DatePicker.defaultProps = defaultProps
-DatePicker.propTypes = propTypes
+// RNInfiniteScroll.defaultProps = defaultProps
+// RNInfiniteScroll.propTypes = propTypes
 
 export default RNInfiniteScroll
